5615e5519b861d56ac3ebdea0ed998e7037813a4
gdenisaaa <146658242+gdenisaaa@users.noreply.github.com>
2023-10-03 19:59:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 const card = document.querySelector('.card');
-card.addEventListener('click', 
-function(){
-this.classList.toggle('flipped');
-});
-
 const cardFront = document.querySelector('.card-front');
 const cardBack = document.querySelector('.card-back');
 
-cardFront.addEventListener('click',
-function(event) {
-    event.stopPropagation();
+card.addEventListener('click', 
+function(){
+card.classList.toggle('flipped');
 });
 
-cardBack.addEventListener('click', function() {
+cardBack.addEventListener('click', function(event) {
+    event.stopPropagation();
     card.classList.toggle('flipped');
 });
